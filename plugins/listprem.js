733cb949618e77c1d68a80f8ecd72df3e0485591
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-25 11:42:36 -0500

    Update listprem.js

--- plugins/listprem.js
+++ plugins/listprem.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn, isOwner }) => {
   let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `┌〔 Daftar Pengguna Premium 〕` + `\n` + prem.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: prem } })
+  conn.reply(m.chat, `┌〔 💎 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 💎 〕` + `\n` + prem.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: prem } })
 }
 handler.help = ['listprem']
 handler.tags = ['info']
