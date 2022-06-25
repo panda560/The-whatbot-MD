3e5a45420514998dc71b9bb5ef7375b17d566958
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-25 14:31:29 -0500

    Create menu.1.js

---
+++ plugins/menu.1.js
@@ -0,0 +1,60 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let duit = `*╭───── 「 𝐒𝐔𝐏𝐄𝐑𝐁𝐎𝐓 - 𝐌𝐃 」 ──────*
+│ *hola %name* *bienvenido este bot ds nuevl todavía esta el desarrollo por eso sin algun comando falla no dude el infoma al creador*
+│🔸 tiempo activo: %uptime
+│🔸 modo: ${global.opts['self'] ? 'privado' : 'publico'}
+│🔸 Version: %version
+│🔸 Library: Baileys-MD
+└────
+╭─「 %category 」
+│ • 
+╰────\n`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/ca517a39ce85c8a4c0479.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🏧 Saweria',
+               url: 'https://saweria.co/ilmanhdyt'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: 'Telkomsel',
+               phoneNumber: '+62 813-5104-7727'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^menu(com|ple)|allmenuu$/i
+
+module.exports = handler
