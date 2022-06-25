be1d3a1441ed581e1a5e459c7c7694dc0c3c0326
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-25 15:34:44 -0300

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,19 +1,20 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.db.data.chats[m.chat];
-  let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
-  if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup) {
-    let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
-    if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
-    await conn.reply(m.chat, `*ANTILINK DECTETADOi!*${isBotAdmin ? '' : '\n\nusted seda explusado del grupo'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *.on restrict* supaya bisa kick'}`, '', '', '', m)
-    if (global.opts['restrict']) {
-      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
-    }
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+
+handler.before = function (m, { user, isAdmin, isBotAdmin }) {
+
+  if (m.isBaileys && m.fromMe) throw false
+  let chat = global.DATABASE.data.chats[m.chat]
+  let name = this.getName(m.sender)
+  let link = linkRegex.exec(m.text)
+
+  if (chat.antiLink && link && !isAdmin && !m.isBaileys && m.isGroup && !m.fromMe) {
+  if (user.isAdmin || user.isSuperAdmin) return m.reply ('Te salvaste gil eres admin')
+ m.reply(`*「 ANTILINK DETECTADO 🤑」*\nno se permite enlace Lo siento *${name}* Has enviado el enlace de tu grupo. Seras explusado. 🤺`)
+   this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
   }
-  return true
 }
+handler.group = true
 
 module.exports = handler
