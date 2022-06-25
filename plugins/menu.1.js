5f7d6f80aa3a031a543f8bdd94cfae74ab475b7f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-25 18:37:43 -0500

    Update menu.1.js

--- plugins/menu.1.js
+++ plugins/menu.1.js
@@ -4,12 +4,66 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let handler = async (m) => {
 let duit = `*╭───── 「 𝐒𝐔𝐏𝐄𝐑𝐁𝐎𝐓 - 𝐌𝐃 」 ──────*
-│ hola @user bienvenido este bot es nuevo todavía está el desarrollo por eso sin algun comando falla no dude el informa al creador
+│ hola ${name} bienvenido este bot es nuevo todavía está el desarrollo 
 │ 🔸*menuprueba*
 └────
-╭─「 %category 」
+╭─「 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 」
 │ • 
-╰────\n`
+╰────
+╭─「 𝙹𝚄𝙴𝙶𝙾𝚂 」
+│ • 
+╰──── 
+╭─「 𝙰𝙳𝚅𝙴𝙽𝚃𝚄𝚁𝙰 」
+│ • 
+╰────
+╭─「 𝙽𝙸𝚅𝙴𝙻 & 𝙴𝚇𝙿 」
+│ • 
+╰────
+╭─「 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 」
+│ • 
+╰────
+╭─「 𝙶𝚁𝚄𝙿𝙾  」
+│ • 
+╰────
+╭─「  𝙰𝚄𝚂𝙴𝙽𝙲𝙸𝙰𝚂 」
+│ • 
+╰────
+╭─「 𝚅𝙾𝚃𝙰𝙲𝙸𝙾𝙽  」
+│ • 
+╰────
+╭─「  𝙵𝚄𝙽 」
+│ • 
+╰────
+╭─「 𝚂𝚃𝙸𝙲𝙺𝙴𝚁  」
+│ • 
+╰────
+╭─「 𝙼𝙰𝚁𝙺𝙴𝚃  」
+│ • 
+╰────
+╭─「 𝙶𝙸𝚃𝙷𝚄𝙱  」
+│ • 
+╰────
+╭─「 𝙸𝙽𝚃𝙴𝚁𝙽𝙴𝚃  」
+│ • 
+╰────
+╭─「 𝙰𝙽𝙸𝙼𝙴  」
+│ • 
+╰────
+╭─「 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚂  」
+│ • 
+╰────
+╭─「 𝙽𝚂𝙵𝚆  」
+│ • 
+╰────
+╭─「 𝚃𝙾𝙾𝙻𝚂  」
+│ • 
+╰────
+╭─「 𝙰𝙳𝚅𝙰𝙽𝙲𝙴  」
+│ • 
+╰────
+╭─「  𝙾𝚆𝙽𝙴𝚁 」
+│ • 
+╰────`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/10842a031f39684aa2297.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
@@ -35,6 +89,16 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
                displayText: 'descarga',
                id: '.descarga',
              }
+             {
+             quickReplyButton: {
+               displayText: 'descarga',
+               id: '.descarga',
+             }
+             {
+             quickReplyButton: {
+               displayText: 'descarga',
+               id: '.descarga',
+             }
 
            }]
          }
@@ -50,6 +114,6 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
 
 handler.help = ['prueba']
 handler.tags = ['info']
-handler.command = /^prueba(com|ple)|prueba$/i
+handler.command = /^prueba(com|ple)|allmenu$/i
 
 module.exports = handler
