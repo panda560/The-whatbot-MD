8b0a11838fe3842eb102316622cbad7737daf996
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-25 10:31:41 -0500

    Add files via upload

--- plugins/update.js
+++ plugins/update.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, isROwner }) => {
 }
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = /^actualizar$/i
+handler.command = /^update$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
