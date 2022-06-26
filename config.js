20ac337d2516da7df088f012898e8db4b9a397d8
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-26 08:54:13 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,58 +1,87 @@
-let fs = require('fs')
+//=============『 Utama 』================== //
+global.owner = ['56977774748' , '51935116539'] 
+global.mods = [] 
+global.prems = [] 
 
-global.owner = ['56977774748' , '51935116539' , '447441419651'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
+//=============『 Info Owner 』============== //
+global.nameowner = 'superbot-MD'
+global.numberowner = '56977774748'
+global.instagram = '.'
+global.github = '.'
+global.dana = '087892711054'
+global.pulsa = '085212023766'
+global.gopay = '087892711054'
+
+//=============『 Info Bot 』=================//
+global.namebot = 'superbot-MD'
+global.gc = 'bit.ly/AnyaBot-Group'
+global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+
+//=======『 Tampilan Dan Lainnya 』============//
+global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.wm = 'ꜱʜɪʀᴏ ɪᴋᴀꜱᴀᴋɪ ʙᴏᴛ'
+global.watermark = wm
+global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
+global.wm3 = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.htki = '––––––『' 
+global.htka = '』––––––'
+global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wait = '_*tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = 'Stiker sedang dibuat'
+global.packname = '2022'
+global.author = '© Hyzer Official'
+
+//=============『 Apikey 』================== //
 global.APIs = { // API Prefix
   // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
-  bg: 'http://bochil.ddns.net',
   xteam: 'https://api.xteam.xyz',
-  melcanz: 'httpa://melcanz.com',
-  lol: 'https://api.lolhuman.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
-  pencarikode: 'https://pencarikode.xyz',
-  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'apivproject',
   'https://melcanz.com': 'elaina',
-  'https://api.lolhuman.xyz': 'pkebgk8248jskrkfm',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
   'https://api.zeks.xyz': 'apivinz',
-  'https://pencarikode.xyz': 'pais',
-  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
 }
 
-// Sticker WM
-global.stiker_wait = 'espere unos minutos'
-global.packname = 'toma tu sticker'
-global.author = 'superbot-MD'
-global.namabot = 'superbot-MD'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ShiraoriBOT-Md • Ilmanhdyt'
-global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
-
-global.wait = '_*tunggu sedang di proses...*_'
-global.eror = '_*Server Error*_'
-
-global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = ".._"
-
-//============= callButtons =============//
-global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '56977774748'
-
-//============= Games ================//
-global.benar = '_*Benar✅*_'
-global.salah = '_*Salah❌*_'
-global.dikit = "dikit lagi, semangat ya :')"
-
-
+//=============『 RPG GAMES 』================== //
 global.multiplier = 69 // The higher, The harder levelup
-
 global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
@@ -85,7 +114,8 @@ global.rpg = {
   }
 }
 
-
+//===========『 Jangan Di Ubah 』================ //
+let fs = require('fs')
 let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
