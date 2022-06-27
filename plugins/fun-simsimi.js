2c706a94d3a3700f09346fc71c53b197cd58e4ca
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 08:16:59 -0500

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
 handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i
 
 module.exports = handler
 
