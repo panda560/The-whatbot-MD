f6dd98f473747e4d1773947ad3cc270fab1fa50f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 17:41:30 -0300

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -2,7 +2,7 @@ let limit = 1024
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  if (!args || !args[0]) throw 'Ufff. Que está buscado 🤔?'
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
