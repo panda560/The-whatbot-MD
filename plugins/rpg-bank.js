ae0890507b9d9bea10149c41e977024ee791543f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 06:59:22 -0500

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,21 +1,51 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
-let handler = async (m, { conn }) => {
+let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    let name = conn.getName(m.sender)
-    let thumbnail = fs.readFileSync('./media/bank.jpg')
     let user = global.db.data.users[who]
-let anu = `
-🏦 Bank *${user.name}*
-⭐ Role : *${user.role}*\n\n
-*${user.exp}* Exp ✨
-*${user.limit}* Limit 📊
-*${user.money}* Money 💵`
-      conn.sendButtonImg(m.chat, thumbnail, anu, wm2, 'Menu', '.menu', m) 
+let anu = `╭───❑ 「 𝐁𝐀𝐋𝐀𝐍𝐂𝐄 」 ❑────
+│ ✇ 𝙽𝚘𝚖𝚋𝚛𝚎: ${user.name}
+│ ✇ 𝙻𝚒𝚖𝚒𝚝𝚎: ${user.limit}
+│ ✇ 𝙳𝚒𝚗𝚎𝚛𝚘: ${user.money}
+│ ✇ 𝙴𝚇𝙿: ${user.exp}
+│ ✇ 𝙽𝚒𝚟𝚎𝚕: ${user.level}
+│ ✇ 𝚁𝚊𝚗𝚐𝚘: ${user.role}
+╰❑`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/bank.jpg') }, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '💠 ɢɪᴛʜᴜʙ',
+               url: 'https://github.com/ilmanhdyt/ShiraoriBOT-Md'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: '🔙 ᴍᴇɴᴜ',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
-handler.help = ['bank', 'dompet', 'dompet @user']
-handler.tags = ['xp', 'rpg']
-handler.command = /^(my|dompet)$/i
+handler.help = ['my', 'my @user']
+handler.tags = ['xp']
+handler.command = /^(my)$/i
 
 module.exports = handler
