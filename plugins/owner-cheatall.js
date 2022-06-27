6fc3e85308904619f170c3ea7c958928f6b0bb82
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 13:53:36 -0500

    Update owner-cheatall.js

--- plugins/owner-cheatall.js
+++ plugins/owner-cheatall.js
@@ -2,7 +2,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
-        conn.reply(m.chat, `*Succes !*`, m)
+        conn.reply(m.chat, `*Listo para comprobar escriba .my*`, m)
         global.db.data.users[m.sender].money = Infinity
         global.db.data.users[m.sender].limit = Infinity
         global.db.data.users[m.sender].level = Infinity
