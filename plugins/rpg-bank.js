fe4108857c8bbacff871110352303b389f69eb99
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 07:08:32 -0500

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -6,13 +6,17 @@ let handler = async (m) => {
     else who = m.sender
     let user = global.db.data.users[who]
 let anu = `╭───❑ 「 𝐁𝐀𝐋𝐀𝐍𝐂𝐄 」 ❑────
-│ ✇ 𝙽𝚘𝚖𝚋𝚛𝚎: ${user.name}
-│ ✇ 𝙻𝚒𝚖𝚒𝚝𝚎: ${user.limit}
-│ ✇ 𝙳𝚒𝚗𝚎𝚛𝚘: ${user.money}
-│ ✇ 𝙴𝚇𝙿: ${user.exp}
-│ ✇ 𝙽𝚒𝚟𝚎𝚕: ${user.level}
-│ ✇ 𝚁𝚊𝚗𝚐𝚘: ${user.role}
-╰❑`
+├─ 📇 *𝙽𝙾𝙼𝙱𝚁𝙴*: ${user.name}
+├─ 🆔 *𝙽𝚄𝙼𝙴𝚁𝙾* : ${require('awesome-phonenumber')(`+${m.sender.split('@')[0]}`).getNumber('international')}
+├─ 🎨 *𝙴𝙳𝙰𝙳* : ${user.registered ? '' + user.age : ''}
+├─ 🎫 *𝙻𝙸𝙼𝙸𝚃𝙴*: ${user.limit}
+├─ 💹 *𝙳𝙸𝙽𝙴𝚁𝙾*: ${user.money}
+├─ 📍 *𝚁𝙰𝙽𝙶𝙾*: ${user.role}
+├─ 💳 *B𝙱𝙰𝙽𝙲𝙾* : ${user.atm}
+├─ 🎟 ️ *𝙻𝙸𝙼𝙸𝚃 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾* : ${user.glimit}
+├─ 📊 *𝙽𝙸𝚅𝙴𝙻*: ${user.level}
+╰─❑ ✨ *𝙴𝚇𝙿*: ${user.exp}
+`
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
