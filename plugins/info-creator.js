1ea60557ac5cb152f1963a2247c821230ee474a5
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 17:50:40 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,5 +1,6 @@
 let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
+const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
