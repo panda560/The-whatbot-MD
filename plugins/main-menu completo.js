70c8c493d61802a85afbcccd8751c6f800b123e4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-26 19:19:54 -0500

    Add files via upload

---
+++ plugins/main-menu completo.js
@@ -0,0 +1,54 @@
+/** 
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let duit = `*╭───── 「 𝐒𝐔𝐏𝐄𝐑𝐁𝐎𝐓 - 𝐌𝐃 」 ──────*
+│ *menu completo*
+└────
+╭─「 %category 」
+│ • 
+╰────\n`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/10842a031f39684aa2297.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'grupo',
+               url: 'https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: '',
+               phoneNumber: ''
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['full']
+handler.tags = ['info']
+handler.command = /^full(com|ple)|full$/i
+
+module.exports = handler
