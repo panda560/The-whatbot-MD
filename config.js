d20e01014e9f153e45b467c4447034d1d3069782
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-27 17:29:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.pulsa = '085212023766'
 global.gopay = '087892711054'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'superbot-MD'
+global.namebot = 'The whatbot-MD'
 global.gc = 'bit.ly/AnyaBot-Group'
 global.web = 'https://github.com/Hyzerr' //conviértalo en su sitio web, puede vincular ig, vincular github, vincular yt, si el enlace es gc, la visualización volverá a ser diferente.
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //cambia a
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //cambiar esto también
 global.lolkey = 'Papah-Chan' //asi que es facil remplazarlo, por ejemplo el apikey esta caducado :v
 global.zenzkey = 'BagasPrdn' //cambiar a su apikey si caduca
-global.wm = '𝑺𝑼𝑷𝑬𝑹𝑩𝑶𝑻 - 𝑴𝑫'
+global.wm = 'The whatbot-MD | 𝑺𝑼𝑷𝑬𝑹𝑩𝑶𝑻 - 𝑴𝑫'
 global.watermark = wm
-global.wm2 = '                     「 𝑺𝑼𝑷𝑬𝑹𝑩𝑶𝑻 - 𝑴𝑫 」'
-global.wm3 = '𝚂𝚄𝙿𝙴𝚁𝙱𝙾𝚃 - 𝙼𝙳'
+global.wm2 = '                     「 The whatbot-MD | 𝑺𝑼𝑷𝑬𝑹𝑩𝑶𝑻 - 𝑴𝑫 」'
+global.wm3 = 'The whatbot-MD | 𝚂𝚄𝙿𝙴𝚁𝙱𝙾𝚃 - 𝙼𝙳'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/fed1c9d94b37511e570d5.mp4'
@@ -36,8 +36,8 @@ global.eror = '_*𝙴𝚛𝚛𝚘𝚛 𝚍𝚎𝚕 𝚂𝚎𝚛𝚟𝚒𝚍𝚘�
 global.benar = '𝙲𝚘𝚛𝚛𝚎𝚌𝚝𝚘 ✅\n'
 global.salah = '𝙴𝚚𝚞𝚒𝚟𝚘𝚌𝚊𝚍𝚘 ❌\n'
 global.stiker_wait = '𝚂𝚎 𝚎𝚜𝚝𝚊 𝚌𝚛𝚎𝚊𝚗𝚍𝚘 𝚎𝚕 𝚜𝚝𝚒𝚌𝚔𝚎𝚛...'
-global.packname = '2022'
-global.author = '© Hyzer Official'
+global.packname = 'toma tu sticker The whatbot-MD'
+global.author = '© 𝚂𝚄𝙿𝙴𝚁𝙱𝙾𝚃 - 𝙼𝙳''
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
