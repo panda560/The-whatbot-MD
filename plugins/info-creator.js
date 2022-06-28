a1e9f13bdb73c72449ba81d52e661e955bd47916
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-28 16:11:40 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,9 +1,17 @@
-let handler = async(m, { conn }) => {
-const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+                    + 'VERSION:3.0\n' 
+                    + 'N:;Ilman;;;'
+                    + 'FN:Ilman\n' // full name
+                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6281351047727:+62 813-5104-7727\n' // WhatsApp ID + phone number
+                    + 'END:VCARD'
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Ilman', contacts: [{ vcard }] } }, { quoted: m })
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner']
 handler.tags = ['info']
-handler.command = /^(owner|creator)$/i
+
+handler.command = /^(owner|creator|propietario|creador|dueño)$/i
 
 module.exports = handler
