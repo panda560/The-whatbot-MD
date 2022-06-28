b13a2dfb024e358958d9b7c8ed475589d4e932e6
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-28 07:00:17 -0500

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -5,7 +5,7 @@ let handler = async (m, { usedPrefix, text }) => {
         throw `_*𝙰𝚞𝚗 𝚑𝚊𝚢 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚌𝚑𝚊𝚝!*_\n\n*${usedPrefix}delasitencia* - 𝙿𝚊𝚛𝚊 𝚋𝚘𝚛𝚛𝚊𝚛 𝚕𝚊 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊`
     }
     conn.absen[id] = [
-        m.reply(`Berhasil memulai absen!\n\n*${usedPrefix}absen* - untuk absen\n*${usedPrefix}cekabsen* - untuk mengecek absen\n*${usedPrefix}hapusabsen* - untuk menghapus data absen`),
+        m.reply(`𝙸𝚗𝚒𝚌𝚒𝚘 𝚌𝚘𝚗 𝚎𝚡𝚒𝚝𝚘\n\n*${usedPrefix}presente* - 𝙿𝚊𝚛𝚊 𝚎𝚜𝚝𝚊𝚛 𝚙𝚛𝚎𝚜𝚎𝚗𝚝𝚎\n*${usedPrefix}cekabsen* - 𝙿𝚊𝚛𝚊 𝚌𝚘𝚖𝚙𝚛𝚘𝚋𝚊𝚛 𝚕𝚊 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊\n*${usedPrefix}delasistencia* - 𝙿𝚊𝚛𝚊 𝚋𝚘𝚛𝚛𝚊𝚛 𝚕𝚘𝚜 𝚍𝚊𝚝𝚘𝚜 𝚍𝚎 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊`),
         [],
         text
     ]
