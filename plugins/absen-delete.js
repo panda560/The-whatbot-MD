1e03c0c05dc1a01b4594e0ddbabcfb1bed4e7593
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-28 07:02:24 -0500

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -1,13 +1,13 @@
 let handler = async (m, { usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) conn.sendBut(m.chat, `_*¡𝙽𝚘 𝚑𝚊𝚢 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚐𝚛𝚞𝚙𝚘!*_\n\n*${usedPrefix}asistencia* - 𝚎𝚖𝚙𝚎𝚣𝚊𝚛 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊`, wm, 'ᴇᴍᴘᴇᴢᴀʀ', '.asistencia', m)
+    if (!(id in conn.absen)) throw `_*¡𝙽𝚘 𝚑𝚊𝚢 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚐𝚛𝚞𝚙𝚘!*_\n\n*${usedPrefix}asistencia* - 𝙿𝚊𝚛𝚊 𝚎𝚖𝚙𝚎𝚣𝚊𝚛 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊`
     delete conn.absen[id]
     m.reply(`𝚂𝚎 𝚎𝚕𝚒𝚖𝚒𝚗𝚘 𝚕𝚊 𝚊𝚜𝚒𝚜𝚝𝚎𝚗𝚌𝚒𝚊 𝚍𝚎 𝚎𝚜𝚝𝚎 𝚐𝚛𝚞𝚙𝚘!`)
 }
 handler.help = ['hapusabsen']
 handler.tags = ['absen']
-handler.command = /^(delete|hapus)absen$/i
+handler.command = /^(del|hapus)asistencia$/i
 handler.group = true
 handler.admin = true
 module.exports = handler
