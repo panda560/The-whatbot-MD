b3915f95aefbcac999d5a1823578814c93dcdff4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-28 07:19:19 -0500

    Update group-afk.js

--- plugins/group-afk.js
+++ plugins/group-afk.js
@@ -1,19 +1,15 @@
-let handler = (m, { text }) => {
-  let user = global.db.data.users[m.sender]
-  user.afk = + new Date
-  user.afkReason = text
-  let str = `╭──[ *𝐀𝐅𝐊 !* ]──✧
+let handler = async(m, { text }) => {
+    let user = global.db.data.users[m.sender]
+    user.afk = +new Date
+    user.afkReason = text
+    m.reply(`╭──[ *𝐀𝐅𝐊 !* ]──✧
 ┆ *𝙽𝚘𝚖𝚋𝚛𝚎* : ${conn.getName(m.sender)}
 ┆ *𝙼𝚘𝚝𝚒𝚟𝚘* : ${text ? '' + text : ''}
 ╰┅────★
-`.trim()
-conn.sendButton(m.chat, str, wm, 'Jangan Ganggu Ya !', 'iyaaaaa',m)
-conn.reply(str)
+`)
 }
-handler.help = ['afk <alasan>']
-handler.tags = ['group']
+handler.help = ['afk [alasan]']
+handler.tags = ['main']
 handler.command = /^afk$/i
 
 module.exports = handler
-
-let wm = global.botwm
