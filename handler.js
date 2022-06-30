d943d6a557ef3c07eb99fa76218d1d4cad8a0c16
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-30 09:56:49 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -708,7 +708,10 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'se unio, bienvenido wey').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'se fue, unos menos 1'))
-                                this.sendButtonImg(id, pp, text, "grupos", "xd", "wkwk", null)
+                                this.sendHydrated(id, text, groupMetadata.subject, apii.data, 'https://github.com/elrebelde21/The-whatbot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+                            [(action == 'add' ? '𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙾' : '𝙰𝙳𝙸𝙾𝚂'), '...'],    
+                            ['𝙼𝙴𝙽𝚄', '/menu']
+                            ], '', { mentions: [user]})
                                 }
                     }
                 }
