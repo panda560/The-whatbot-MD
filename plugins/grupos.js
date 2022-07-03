654e3424ffc3439daf2ab306b32f059092033f92
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-07-03 11:01:44 -0300

    Add files via upload

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -7,10 +7,73 @@ const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessa
 let handler = async (m) => {
 let duit = `*────── 「 GRUPO」 ──────*
 
-*Hola 👋*
-*estos sos los grupo oficial del bot*
+*Hola 👋 estos sos los grupos del bot*
+
+𝙂𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 𝙙𝙚𝙡 𝘽𝙤𝙩:
 *https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k*
-h`
+
+*https://chat.whatsapp.com/CFSvSOGCnuL3vk4Js1DWI2*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+
+*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+
+*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+
+*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+
+*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
+
+*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
+
+*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k6J2Dwx7F
+
+*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
+
+*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
+
+*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
+
+*11.-* https://chat.whatsapp.com/GKdDtQKWP5LCgpbzucz6kt
+
+*12.-* https://chat.whatsapp.com/DZA9djOzBn51ZpsCBj6aAg
+
+ 𝙜𝙧𝙪𝙥𝙤 𝙙𝙚 𝙖𝙢𝙞𝙨𝙩𝙖𝙙𝙚𝙨 𝙙𝙤𝙣𝙙𝙚 𝙚𝙨𝙩𝙖 𝙚𝙡 𝙗𝙤𝙩
+
+*bot princess Kiara* 
+https://chat.whatsapp.com/DRqFS8O6Mgx5oJR8o7XffC
+
+*ஓீ͜ঔৣ͡🍒𝐥𝐚 𝐛𝐨𝐭 𝐜𝐞𝐫𝐞𝐳𝐚ঔৣ͡ஓ*
+https://chat.whatsapp.com/DO1IQunLpr73RTXy21Ucak
+
+*amistad y los q pinte*
+https://chat.whatsapp.com/GPXwomXZIGn9rQsHZtPeAv
+
+*᳄⃢🇦🇴‐⃬⃟ʟɪʟ ᴘΣᵉp🪦⃤⃢🏴‍☠️࿐*
+https://chat.whatsapp.com/LPGubFHHuXuCuMoyQjQ8iZ
+
+*꧁⁣༒𓆩Aᴹᴵᴳᴼˢ Cᴴᴬᵀˢ Y Aᴾᴼᴿᵀᴱs*
+https://chat.whatsapp.com/FRpx4wNuUqh84QojoGExUn
+
+*◄≛⁆✤🍒A͍M͍I͍S͍T͍A͍D͍E͍S͍💜✠*
+https://chat.whatsapp.com/LQKjh122sx693mlHyNg25h
+
+ 𝙜𝙧𝙪𝙥𝙤𝙨 𝙙𝙚 𝙚𝙣𝙡𝙖𝙘𝙚 𝙖𝙘𝙖 𝙥𝙤𝙙𝙚𝙧 𝙚𝙣𝙘𝙤𝙣𝙩𝙧𝙖𝙧 𝙜𝙧𝙪𝙥𝙤 𝙮 𝙢𝙖𝙣𝙙𝙖 𝙩𝙪 𝙚𝙣𝙡𝙖𝙘𝙚
+*꧁⃢⃟҉💫🇭🇳🌺ঔENLACES LA JEFA 🇭🇳*
+https://chat.whatsapp.com/CR8piZaPIHYGwKoWrWnMSs
+
+*🌙ꦿꦼ꙲𝐄𝐍𝐋𝐀𝐂𝐄𝐒ꦿꦼ.᭄𝐁𝐈𝐏𝐎𝐋𝐀𝐑 ꦿ♥️*
+https://chat.whatsapp.com/LBFKpTYKhDSGG24i7yNspa
+
+*🌹⃟♚꙱‹🅻🅸🅽🅺🆂𝐂𝐡𝐚𝐭❦⃤𝙴𝙻𝙸𝚉𝙰𝙱𝙴𝚃𝙷*
+https://chat.whatsapp.com/Dq9v557KgJsLtdkK3CALzL
+
+*│🌷.ꦿ│𓏲⊹𝐀𝐌𝐈𝐒𝐓𝐀𝐃𝐄𝐒 𖥨⊹*
+https://chat.whatsapp.com/HTeeFKFAHsP1b0YUm5eDRm
+*grupos asistencia de facebook*
+https://facebook.com/groups/721802642266362/\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+ 𝙨𝙞 𝙦𝙪𝙞𝙚𝙧𝙚 𝙦𝙪𝙚 𝙩𝙪 𝙜𝙧𝙪𝙥𝙤 𝙖𝙥𝙖𝙧𝙚𝙯𝙘𝙖 𝙖𝙘𝙖 𝙝𝙖𝙗𝙡𝙖 𝙘𝙤𝙣 𝙚𝙡 𝙖𝙙𝙢𝙞𝙣 𝙤𝙛𝙘 𝙙𝙚𝙡 𝙗𝙤𝙩`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/e1e0f0bc5a5229c348eda.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
