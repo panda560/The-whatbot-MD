ea5eee28ca06be8217fac765b32efbc501f1b83b
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-07-04 20:06:32 -0300

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn, isROwner, text }) => {
     let anu = groups.map(v => v.id)
     var pesan = m.quoted && m.quoted.text ? m.quoted.text : text
     if(!pesan) throw 'teksnya?'
-    m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5 } detik`)
+    m.reply(`difuncion enviada al ${anu.length} ya,difuncion terminada el ${anu.length * 0.5 } detik`)
     for (let i of anu) {
     await delay(500)
-    conn.sendBut(i, `${pesan}`, wm, 'OWNER', '.owner', null).catch(_ => _)
+    conn.sendBut(i, `${pesan}`, wm, 'owner', '.owner', null).catch(_ => _)
     }
-  m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
+  m.reply(`difuncion enviada con éxitos al ${anu.length} Group`)
 }
 handler.help = ['bcgcbot <teks>']
 handler.tags = ['owner']
