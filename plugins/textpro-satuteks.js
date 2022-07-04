a1f15485d4089e06d35fd4dbcf0ea795242db2ee
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-07-04 20:10:59 -0300

    Update textpro-satuteks.js

--- plugins/textpro-satuteks.js
+++ plugins/textpro-satuteks.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw 'Masukkan Text... '
+if (!text) throw 'ingrese el Texto... '
 let nyenye = `https://api.lolhuman.xyz/api/textprome/${command}?apikey=${lolkey}&text=${text}`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'siguiente', `.${command}`, m) 
 }
 handler.help = ['blackpink', 'neon', 'greenneon', 'futureneon', 'sandwriting', 'sandsummer', 'sandengraved', 'metaldark', 'neonlight', 'holographic', 'text1917', 'minion', 'deluxesilver', 'newyearcard', 'bloodfrosted', 'hallowen', 'fireworksparkle', 'natureleaves', 'bokeh', 'toxic', 'strawberry', 'box3d', 'roadwarning', 'breakwall', 'icecold', 'luxury', 'cloud', 'summersand', 'horrorblood', 'thunder', 'magma', 'impressiveglitch', 'harrypotter', 'foggywindow', 'watercolor', 'wonderfulgrafitti'] 
 handler.tags = ['textpro']
